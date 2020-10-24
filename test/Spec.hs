c228d69bf1f97b91c443fcd790bd8dba638c3085
Isaac-DeFrain <isaacdefrain@gmail.com>
2020-10-24 13:37:01 -0600

    Update and add tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,11 +2,12 @@
 
 import qualified Data.Map as Map
 import Lib
+import RegExp
+import Thompson
 
 -- import RegExp
 import Test.Hspec
 import qualified Test.QuickCheck as QC
-import Thompson
 
 -- TODO: use QC.quickCheckWith
 main :: IO ()
@@ -17,15 +18,27 @@ main =
                 QC.forAll genDFA $ \d -> do
                     w <- genWord
                     pure $ accept d w == accept (minimize d) w
-            it "should be equivalent to original1 -- TODO: fix equivalentDFA" $
+            it
+                "should be equivalent to original DFA 1 -- TODO: fix equivalentDFA" $
                 True `shouldBe` True
-            --     QC.forAll genDFA $ \d -> equivalentDFA d $ minimize d
-            it "should be equivalent to original2 -- TODO: fix equivalentDFA" $
+                -- QC.forAll genDFA $ \d -> equivalentDFA d $ minimize d
+            it
+                "should be equivalent to original DFA 2 -- TODO: fix equivalentDFA" $
                 True `shouldBe` True
             --     QC.forAll genDFA $ \d -> equivalentDFA (minimize d) d
             it "should be minimum -- TODO: fix minimize" $ True `shouldBe` True
             --     QC.forAll genDFA $ \d ->
             --         minimize d == minimize (minimize d)
+            it
+                "should be equivalent to original NFA 1 -- TODO: fix equivalentDFA" $
+                True `shouldBe` True
+                -- QC.forAll genNFA $ \n ->
+                --     let d = nfaToDFA n in equivalentDFA d $ minimize d
+            it
+                "should be equivalent to original NFA 2 -- TODO: fix equivalentDFA" $
+                True `shouldBe` True
+            --     QC.forAll genNFA $ \n ->
+            --      let d = nfaToDFA n in equivalentDFA (minimize d) d
         describe "DFAs" $ do
             it "do not have epsilon transitions" $
                 QC.forAll genDFA $ \(D (DFA _ _ t _)) ->
@@ -56,11 +69,22 @@ main =
                     n <- QC.choose (0, 10)
                     str <- QC.vectorOf n $ QC.choose (c, c)
                     pure $ accept (convert $ c : "*") str
-            it "equivalence is reflexive -- TODO: fix equivalentDFA" $
-                True `shouldBe` True
-                -- QC.forAll genNFA $ \n -> equivalentDFA n n
+            it "equivalence should be reflexive" $
+                QC.forAll genNFA $ \n ->
+                    let d = nfaToDFA n
+                     in equivalentDFA d d
+        describe "REs" $ do
+            it "Concat should be right associative" $
+                QC.forAll genConcat $ \c -> c == simplify c
+            it "should recognize all words in recognizeSample" $
+                QC.forAll genRE $ \r -> recognizeRE r <$> genRecognizeSample r
+            -- it "generated NFA should accept the same words as original" $
+            --     QC.forAll genRE $ \r -> accept (convertRE r) <$> genRecognizeSample r
 
 -- utils
+genRE :: QC.Gen RE
+genRE = QC.arbitrary
+
 genDFA :: QC.Gen (Automaton DFA)
 genDFA = QC.arbitrary
 
@@ -75,6 +99,40 @@ genWord = do
     n <- QC.choose (0, 20)
     QC.vectorOf n $ QC.arbitrary `QC.suchThat` (/= eps)
 
+genWordFrom :: String -> QC.Gen String
+genWordFrom "" = pure ""
+genWordFrom s = do
+    n <- QC.choose (0, 5)
+    QC.vectorOf n $ QC.elements s
+
+genRecognizeSample :: RE -> QC.Gen String
+genRecognizeSample r = do
+    let samp = recognizeSampleRE r
+    if null samp
+        then pure ""
+        else QC.elements samp
+
 noDups :: Eq a => [a] -> Bool
 noDups [] = True
 noDups (h:t) = h `notElem` t && noDups t
+
+genConcat :: QC.Gen RE
+genConcat = do
+    n <- QC.choose (2, 20)
+    gen n
+  where
+    gen :: Int -> QC.Gen RE
+    gen 2 = con genConst genConst
+    gen m = con genConst $ gen (m - 1)
+    genConst = Const <$> genChar
+    con gr1 gr2 = do
+        r1 <- gr1
+        Concat r1 <$> gr2
+
+chars :: RE -> String
+chars Empty = ""
+chars (Const c) = [c]
+chars (Concat r1 r2) = chars r1 <> chars r2
+chars (Alt r1 r2) = chars r1 <> chars r2
+chars (Paren r) = chars r
+chars (Star r) = chars r
